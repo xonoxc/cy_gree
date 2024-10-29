c3cc634898e4d88d86aa5135d9127037c194cfe5
arpit <arpit2003y@gmail.com>
2024-10-29 16:36:11 +0530

    chore(fetch):fetch_config_changes

--- src/config/fetch.config.ts
+++ src/config/fetch.config.ts
@@ -1,34 +1,65 @@
-const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL as  string
-import store  from "@/store/token"
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL as string
+import store from "@/store/token"
+import { refreshTokens } from "@/services/token"
+import { auth } from "@/services/auth";
 
 interface FetchOptions extends RequestInit {
-  headers?: Record<string, string>;
+  headers?: Record<string, string>
 }
 
 const defaultConfig: FetchOptions = {
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-	'Authorization': `Bearer ${store.getState().accessToken}`
+    'Authorization': `Bearer ${store.getState().accessToken}`,
   },
   credentials: "include",
 };
 
-export const fetchWithConfig = async (endpoint: string, options: FetchOptions = {}):Promise<Response> => {
-  const config: FetchOptions = {
+
+export const fetchWithConfig = async (
+  endpoint: string,
+  options: FetchOptions = {}
+): Promise<Response> => {
+  let config: FetchOptions = {
     ...defaultConfig,
     ...options,
     headers: {
       ...defaultConfig.headers,
       ...options.headers,
+      'Authorization': `Bearer ${store.getState().accessToken}`, 
     },
   };
 
-  if(!BASE_URL) {
-    throw new Error("Please provide NEXT_PUBLIC_SERVER_URL in .env ")
+  if (!BASE_URL) {
+    throw new Error("Please provide NEXT_PUBLIC_SERVER_URL in .env");
   }
 
-  return await fetch(`${BASE_URL}/api${endpoint}`, config)
+  try {
+    const response = await fetch(`${BASE_URL}/api${endpoint}`, config);
+
+    if (response.status === 401) {
+			const refreshToken = store.getState().refreshToken
+			if(!refreshToken) auth.logout()
+
+            const refreshStatus = await refreshTokens(refreshToken);
+
+			if(refreshStatus){
+				
+      config.headers = {
+        ...config.headers,
+        'Authorization': `Bearer ${store.getState().accessToken}`,
+      }
+
+	}else {
+				auth.logout()
+	}
+      return await fetch(`${BASE_URL}/api${endpoint}`, config)
+    }
+
+    return response;
+  } catch (error) {
+    console.error("Fetch request failed:", error)
+    throw error
+  }
 }
-
-

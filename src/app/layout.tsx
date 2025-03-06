c06e82bd4c9932abfa459039d52805c347b156c9
arpit <arpit2003y@gmail.com>
2025-03-06 23:44:57 +0530

    refactor(querying):added_query_client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,21 @@
 "use client"
 
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
-import { SessionProvider } from "next-auth/react"
 import localFont from "next/font/local"
-import { ImageKitProvider } from "imagekitio-next"
 import "./globals.css"
-import { useCallback } from "react"
-
+import Providers from "@/components/Providers/prodviders"
 const sfpro = localFont({
     src: "../../public/fonts/sf-pro-display_regular.woff2",
 })
-
-const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_IO_ENDPOINT!
-const publicKey = process.env.NEXT_PUBLIC_IMAGEKIT_IO_PUBLIC_KEY!
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
-    const authenticator = useCallback(async () => {
-        try {
-            const res = await fetch("/auth/bucket")
-            if (!res.ok) {
-                throw new Error("Failed to authenticate")
-            }
-
-            return res.json()
-        } catch (error) {
-            throw error
-        }
-    }, [])
-
     return (
         <html lang="en">
             <body className={`${sfpro.className} antialiased`}>
-                <ImageKitProvider
-                    publicKey={publicKey}
-                    urlEndpoint={urlEndpoint}
-                    authenticator={authenticator}
-                >
-                    <SessionProvider refetchInterval={5 * 60}>
-                        <ThemeProvider attribute="class" defaultTheme="dark">
-                            {children}
-                        </ThemeProvider>
-                        <Toaster />
-                    </SessionProvider>
-                </ImageKitProvider>
+                <Providers>{children}</Providers>
             </body>
         </html>
     )
---
+++ src/components/Providers/prodviders.tsx
@@ -0,0 +1,45 @@
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
+import { SessionProvider } from "next-auth/react"
+
+import { queryClient } from "@/lib/query/client"
+import { QueryClientProvider } from "@tanstack/react-query"
+
+import { ImageKitProvider } from "imagekitio-next"
+
+const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_IO_ENDPOINT!
+const publicKey = process.env.NEXT_PUBLIC_IMAGEKIT_IO_PUBLIC_KEY!
+
+const authenticator = async () => {
+    try {
+        const res = await fetch("/auth/bucket")
+        if (!res.ok) {
+            throw new Error("Failed to authenticate")
+        }
+
+        return res.json()
+    } catch (error) {
+        throw error
+    }
+}
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ImageKitProvider
+            publicKey={publicKey}
+            urlEndpoint={urlEndpoint}
+            authenticator={authenticator}
+        >
+            <SessionProvider refetchInterval={5 * 60}>
+                <QueryClientProvider client={queryClient}>
+                    <ThemeProvider attribute="class" defaultTheme="dark">
+                        {children}
+                    </ThemeProvider>
+                </QueryClientProvider>
+                <Toaster />
+            </SessionProvider>
+        </ImageKitProvider>
+    )
+}
+
+export default Providers

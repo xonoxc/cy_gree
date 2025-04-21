4ab7d8787ea2268b57849eef7dc596dbdc4bc6dc
arpit <arpit2003y@gmail.com>
2025-04-21 21:48:25 +0530

    fix(images):optimized_images_not_loading_in_prod

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,32 @@
 import { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    devIndicators: false,
-    experimental: {
-        reactCompiler: true,
-    },
-    pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-    webpack: config => {
-        config.resolve.fallback = {
-            crypto: require.resolve("crypto-browserify"),
-            stream: require.resolve("stream-browserify"),
-            https: require.resolve("https-browserify"),
-            http: require.resolve("http-browserify"),
-            querystring: require.resolve("querystring-browser"),
-            vm: require.resolve("vm-browserify"),
-        }
-        return config
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "ik.imagekit.io",
-                port: "",
-            },
-        ],
-    },
+	devIndicators: false,
+	experimental: {
+		reactCompiler: true,
+	},
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+	webpack: config => {
+		config.resolve.fallback = {
+			crypto: require.resolve("crypto-browserify"),
+			stream: require.resolve("stream-browserify"),
+			https: require.resolve("https-browserify"),
+			http: require.resolve("http-browserify"),
+			querystring: require.resolve("querystring-browser"),
+			vm: require.resolve("vm-browserify"),
+		}
+		return config
+	},
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "ik.imagekit.io",
+				port: "",
+			},
+		],
+	},
 }
 
 export default nextConfig
